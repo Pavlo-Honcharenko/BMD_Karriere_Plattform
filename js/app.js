c34c5b094ece429c97a844fcc356d74165c32326
Pavlo-Goncharenko <82305213+Pavlo-Goncharenko@users.noreply.github.com>
2024-04-18 18:28:25 +0300

    1828

--- js/app.js
+++ js/app.js
@@ -10,9 +10,18 @@
         }
     }), 0);
     document.addEventListener("click", (function(event) {
-        if (event.target.classList.contains("title-text")) {
-            const parentFilterTitle = event.target.closest(".filter__title");
-            if (parentFilterTitle) parentFilterTitle.classList.toggle("_show");
+        const parentFilterTitle = event.target.closest(".filter__title");
+        if (parentFilterTitle && parentFilterTitle.classList.contains("_show")) {
+            const filterTitles = document.querySelectorAll(".filter__title");
+            filterTitles.forEach((title => {
+                title.classList.remove("_show");
+            }));
+        } else if (parentFilterTitle) {
+            const filterTitles = document.querySelectorAll(".filter__title");
+            filterTitles.forEach((title => {
+                title.classList.remove("_show");
+            }));
+            parentFilterTitle.classList.toggle("_show");
         }
     }));
     window["FLS"] = false;
